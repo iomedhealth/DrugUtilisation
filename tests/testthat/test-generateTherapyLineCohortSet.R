d9a0dfd0c617c0be9369bb84e37e3213c82c1246
Gabriel Maeztu <gabriel.maeztu@gmail.com>
2026-09-05 12:23:17 +0200

    Fix CI by adding generateEpisodeCohortSet to pkgdown and bounding LOT test dates

--- tests/testthat/test-generateTherapyLineCohortSet.R
+++ tests/testthat/test-generateTherapyLineCohortSet.R
@@ -59,8 +59,8 @@ test_that("generateTherapyLineCohortSet Multiple Myeloma LOT & Regimen classific
     ),
     cohort_end_date = c(
       obs_p1$start + 50, obs_p1$start + 50, obs_p1$start + 50, obs_p1$start + 50,
-      obs_p1$start + 150, obs_p1$start + 150, obs_p1$start + 150,
-      obs_p2$start + 50, obs_p2$start + 150, obs_p2$start + 150
+      pmin(obs_p1$start + 150, obs_p1$end - 5), pmin(obs_p1$start + 150, obs_p1$end - 5), pmin(obs_p1$start + 150, obs_p1$end - 5),
+      obs_p2$start + 50, pmin(obs_p2$start + 150, obs_p2$end - 5), pmin(obs_p2$start + 150, obs_p2$end - 5)
     )
   )
 
